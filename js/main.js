75fdf668f109056395b15afdb6259ef22d6135bf
Каймонов Владимир <kaivladimirv@gmail.com>
2016-12-16 14:15:11 +0600

    Добавлена загрузка данных игры с сервера

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+import 'whatwg-fetch';
 import Application from './application';
 
-Application.showWelcome();
+const status = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+window.fetch('https://intensive-ecmascript-server-nnpnvhhedl.now.sh/pixel-hunter/questions')
+    .then(status)
+    .then((response) => response.json())
+    .then((data) => {
+      Application.data = data;
+
+      Application.showWelcome();
+    })
+    .catch((errorText) => {
+      Application.showError({
+        title: 'Произошла ошибка при загрузке данных',
+        description: errorText
+      });
+    });
