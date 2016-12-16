99027634b626c8937a91c4ae0911eb33d883f775
Каймонов Владимир <kaivladimirv@gmail.com>
2016-12-16 12:02:42 +0600

    Добавлено использования констант обозначающих типы задача

--- js/game/game-presenter.js
+++ js/game/game-presenter.js
@@ -4,6 +4,7 @@ import TaskDoubleView from './task-double-view';
 import TaskWideView from './task-wide-view';
 import TaskTripleView from './task-triple-view';
 import GameModel from './game-model';
+import {taskTypes} from '../data/game-data';
 import getDataStats from '../data/stats-data';
 import Application from '../application';
 
@@ -108,15 +109,15 @@ class GamePresenter {
     let task = this._model.getTask();
 
     switch (task.type) {
-      case 'double':
+      case taskTypes.TWO_OF_TWO:
         taskContent = new TaskDoubleView(task, this._model.getStats());
         break;
 
-      case 'wide':
+      case taskTypes.TINDER_LIKE:
         taskContent = new TaskWideView(task, this._model.getStats());
         break;
 
-      case 'triple':
+      case taskTypes.ONE_OF_THREE:
         taskContent = new TaskTripleView(task, this._model.getStats());
         break;
 
