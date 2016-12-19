a32c9c28be17d693a24138f9fa48afc8f400671c
Каймонов Владимир <kaivladimirv@gmail.com>
2016-12-19 17:06:05 +0600

    1) Добавлен метод для отображения процесса обработки/получения данных 2) Добавлено геттер возвращающий адрес сервера 3) Добавлена передача имени пользователя в игру

--- js/application.js
+++ js/application.js
@@ -3,8 +3,9 @@ import introElement from './intro-view';
 import greetingElement from './greeting-view';
 import rulesElement from './rules-view';
 import startGame from './game/game-presenter';
-import showStats from './stats/stats-view';
+import statsElement from './stats/stats-view';
 import errorElement from './error-view';
+import loadingElement from './loading-view';
 import getIntroData from './data/intro-data';
 import getGreetingData from './data/greeting-data';
 import getRulesData from './data/rules-data';
@@ -25,19 +26,27 @@ export default class Application {
     renderElement(rulesElement(getRulesData()));
   }
 
-  static showGame() {
-    startGame(gameData);
+  static showGame(userName) {
+    startGame(userName, gameData);
   }
 
-  static showStats(data) {
-    renderElement(showStats(data));
+  static showStats(data, history) {
+    renderElement(statsElement(data, history));
   }
 
   static showError(data) {
     renderElement(errorElement(data));
   }
 
+  static showLoading(data) {
+    renderElement(loadingElement(data));
+  }
+
   static set data(data) {
     gameData = data;
+  }
+
+  static get severUrl() {
+    return 'https://intensive-ecmascript-server-nnpnvhhedl.now.sh/';
   }
 }
