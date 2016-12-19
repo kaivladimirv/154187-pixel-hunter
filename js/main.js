356657fff5c926de4d3d60de1118016016f8d2dd
Каймонов Владимир <kaivladimirv@gmail.com>
2016-12-19 17:08:55 +0600

    1) Метод обработки статуса ajax-запроса перенесён в модуль «util» 2) Добавлено отображение экрана загрузки

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 import 'whatwg-fetch';
+import {validationStatusForGetRequest} from './utils';
 import Application from './application';
 
-const status = (response) => {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  } else {
-    throw new Error(`${response.status}: ${response.statusText}`);
-  }
-};
+Application.showLoading({title: 'Подождите. Идёт загрузка...', description: ''});
 
-window.fetch('https://intensive-ecmascript-server-nnpnvhhedl.now.sh/pixel-hunter/questions')
-    .then(status)
+window.fetch(Application.severUrl + 'pixel-hunter/questions')
+    .then(validationStatusForGetRequest)
     .then((response) => response.json())
     .then((data) => {
       Application.data = data;
